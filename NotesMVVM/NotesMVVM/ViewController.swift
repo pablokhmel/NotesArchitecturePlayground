861d2210ed4c0f03eaceea9bb7bbdb2d3fb0b9bb
Pavel Khmelevskiy <99418150+pablokhmel@users.noreply.github.com>
2026-01-26 13:48:21 +0100

    [MVVM] Created base for MVVM (#12)

---
+++ NotesMVVM/NotesMVVM/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NotesMVVM
+//
+//  Created by Pavel Khmialeuski1 on 22/01/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
