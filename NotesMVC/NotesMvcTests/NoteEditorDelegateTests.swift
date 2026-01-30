e58b0f6e3e250b4ec1775cb0d74bd7fa68cb0f80
Pavel Khmelevskiy <99418150+pablokhmel@users.noreply.github.com>
2026-01-30 13:01:59 +0100

    [MVC] Unit Tests for NoteEditorDelegate (#26)

---
+++ NotesMVC/NotesMvcTests/NoteEditorDelegateTests.swift
@@ -0,0 +1,58 @@
+import XCTest
+@testable import NotesMVC
+import Core
+
+final class NoteEditorDelegateTests: XCTestCase {
+
+    func testMethodsCallable() {
+        final class DefaultMock: NoteEditorDelegate {}
+
+        let mock = DefaultMock()
+        let note = NoteModel(name: "Test", text: "Body", createdDate: Date())
+
+        mock.editedNote(note)
+        mock.createdNote(note)
+    }
+
+    func testCanOverrideBothMethods() {
+        final class OverrideMock: NoteEditorDelegate {
+            var editedCalled = false
+            var createdCalled = false
+
+            func editedNote(_ note: NoteModel) {
+                editedCalled = true
+            }
+
+            func createdNote(_ note: NoteModel) {
+                createdCalled = true
+            }
+        }
+
+        let mock = OverrideMock()
+        let note = NoteModel(name: "X", text: "Y", createdDate: Date())
+
+        mock.editedNote(note)
+        mock.createdNote(note)
+
+        XCTAssertTrue(mock.editedCalled, "Overriding editedNote(_:) should be called")
+        XCTAssertTrue(mock.createdCalled, "Overriding createdNote(_:) should be called")
+    }
+
+    func testCanOverrideSingleMethod() {
+        final class PartialOverrideMock: NoteEditorDelegate {
+            var editedCalled = false
+
+            func editedNote(_ note: NoteModel) {
+                editedCalled = true
+            }
+        }
+
+        let mock = PartialOverrideMock()
+        let note = NoteModel(name: "A", text: "B", createdDate: Date())
+
+        mock.editedNote(note)
+        mock.createdNote(note)
+
+        XCTAssertTrue(mock.editedCalled, "Overridden editedNote(_:) should be called")
+    }
+}
